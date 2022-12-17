a98f1306c1568347d3f9e5940407bac98d988af0
elaineads <elaineaps@outlook.com.br>
2022-12-17 11:54:32 -0300

    Add rota inicial de listagem de jogos

---
+++ jogoteca.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def ola():
+    jogo1 = Jogo('Tetrs', 'Puzze', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombate', 'Luta', 'PS2')
+
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True)
